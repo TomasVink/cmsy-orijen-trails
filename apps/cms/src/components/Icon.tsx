e1a394c016b009d969700a0ab2cb2a65946bd7b7
Tomas Vink <tomasvink@gmail.com>
2026-03-26 09:32:51 +0100

    add logo from s3

--- apps/cms/src/components/Icon.tsx
+++ apps/cms/src/components/Icon.tsx
@@ -1,4 +1,10 @@
 export default function Icon() {
   const base = process.env.NEXT_PUBLIC_SERVER_URL || 'http://localhost:3000'
-  return <img src={`${base}/cmsy_logo.svg`} alt="cmsy logo" style={{ height: '24px' }} />
+  return (
+    <img
+      src="https://cmsy-assets.s3.eu-central-1.amazonaws.com/cmsy_logo.svg"
+      alt="cmsy logo"
+      style={{ height: '24px' }}
+    />
+  )
 }
